6f19ce532a73344013ea8de33b40d88afbfa42df
Mateus Romani <mateusromani@gmail.com>
2022-09-18 17:24:58 -0300

    continuação da resolução

---
+++ ex23/ex23.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int tamanho, *array;
+
+    printf("Digite um tamanho para o array: ");
+    scanf("%i", &tamanho);
+
+    array = malloc(tamanho * sizeof(int));
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        printf("\nDigite um valor inteiro para array[%i]:", i);
+        scanf("%i", array)
+    }
+    
+    return 0;
+}
